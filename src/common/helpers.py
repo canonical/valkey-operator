5356f46349068d2f35894e06ba938042f78552b2
Smail Kourta <smail.kourta@canonical.com>
2026-03-12 09:26:55 +0000

    remove unused helper

--- src/common/helpers.py
+++ src/common/helpers.py
@@ -2,24 +2,7 @@
 # See LICENSE file for licensing details.
 """Helper functions for the charm."""
 
-import ipaddress
 import socket
-
-
-def is_valid_ip(ip_string: str) -> bool:
-    """Check if the provided string is a valid IP address.
-
-    Args:
-        ip_string: The string to check.
-
-    Returns:
-        bool: True if the string is a valid IP address, False otherwise.
-    """
-    try:
-        ipaddress.ip_address(ip_string)
-        return True
-    except ValueError:
-        return False
 
 
 def dotappend(string: str) -> str:
