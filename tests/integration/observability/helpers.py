6085d7d0e889ae2eaa6aece4688a9075f19aec77
Smail Kourta <smail.kourta@canonical.com>
2026-09-15 14:46:22 +0400

    patch: move cos channel to 0.130/stable to support 26.04

--- tests/integration/observability/helpers.py
+++ tests/integration/observability/helpers.py
@@ -17,7 +17,7 @@ from tests.integration.helpers import APP_NAME
 logger = logging.getLogger(__name__)
 
 NUM_UNITS = 3
-COS_CHANNEL = "2/stable"
+COS_CHANNEL = "0.130/stable"
 OTELCOL_K8S_APP = "otelcol-k8s"
 OTELCOL_VM_APP = "otelcol"
 PROMETHEUS_APP = "prometheus"
